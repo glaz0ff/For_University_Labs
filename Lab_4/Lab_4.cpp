cb1227f0aa1af84ceddf6dbe3ddacab18e2ee448
glaz0ff <glazzer0@mail.ru>
2020-12-29 20:56:04 +0500

    new_lab

---
+++ Lab_4/Lab_4.cpp
@@ -0,0 +1,80 @@
+﻿
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+	int num, i, a, j, k, m, ex;
+	cout << "Введите кол-во элементов массива: ";
+	cin >> num;
+
+	int* arr = new int[num];
+	for (i = 0; i < num; i++) {
+		arr[i] = 1 + rand() % INT_MAX;
+	}
+	for (i = 0; i < num; i++) {
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+	cout << endl;
+
+	cout << "Удалить все элеенты кратные 7  и после каждого нечетного элемента добавить 0: ";
+	cout << endl;
+	k = num--;
+	for (i = 0; i < num; i++) {
+		for (a = i; a < num; a++) {
+			if (arr[i] % 7 == 0) {
+				for (j = i; j < k; j++) {
+					arr[j] = arr[j + 1];
+				}
+				arr[num] = 0;
+			}
+			else
+				break;
+		}
+	}
+
+
+	m = 0;
+	
+	for (i = 0; i < num; i++) {
+		
+			if (arr[i] % 2 != 0) {
+				m++;
+			}
+
+	}
+	
+	
+	ex = num + m;
+	int* mur = new int[ex];
+	for (i = 0; i < ex-1; i++) {
+		mur[i] = 0;
+	}
+	mur = arr;
+
+	k = ex - 1;
+	for (j = 0; j < k; j++) {
+		for (a = j; j < k; j++) {
+			if (mur[j] % 2 != 0) {
+				
+				for (i = k; i > j; i--) {
+					mur[i] = mur[i-1];
+				}
+				mur[j + 1] = 0;
+				
+			}
+			break;
+		}
+	}
+	for (i = 0; i < ex - 1; i++) {
+		cout << mur[i] << " ";
+	}
+
+	delete[] mur;
+	delete[] arr;
+	return(0);
+}
---
+++ Lab_7.2/Lab_7.2.cpp
@@ -0,0 +1,28 @@
+﻿
+#include <iostream>
+using namespace std;
+
+
+int sum(int n...) {
+    int s;
+    s = 0;
+    int* p = &n;
+    for (; n != 0; n--) {
+        if (n % 2 == 0) {
+            s -=(*p * (*(p+2)));
+        }
+        else
+            s += (*p * (*(p+2)));
+    }
+    return(s);
+}
+
+int main()
+{
+    cout << sum(5, 2, 1, 6, 3, 4);
+    cout << sum(10, 2, 1, 6, 3, 4, 23, 0, 54, 9, 8);
+    cout << sum(12, 2, 7, 8, 0, 15, 1, 6, 3, 4, 5, 9, 10);
+
+    return(0);
+}
+
